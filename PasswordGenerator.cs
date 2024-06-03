a5c2e9cd5a2c2d53263bd5166dcb21272a5a6495
Wille Persson <144245981+WillePersson@users.noreply.github.com>
2024-06-03 13:33:19 +0200

    Update PasswordGenerator.cs

--- PasswordGenerator.cs
+++ PasswordGenerator.cs
@@ -11,6 +11,7 @@ namespace Design_Patterns_Labb1_PasswordGenerator
 	{
 		private static PasswordGenerator _instance; // Single instance
 		private IPasswordStrategy _strategy; // Strategy pattern interface
+		private static readonly object lockObject = new object();
 
 		private PasswordGenerator() { } // Private constructor
 
@@ -20,7 +21,13 @@ namespace Design_Patterns_Labb1_PasswordGenerator
 			{
 				if (_instance == null)
 				{
-					_instance = new PasswordGenerator(); // Creating the single instance
+					lock (lockObject)
+					{
+						if (_instance == null)
+						{
+						     _instance = new PasswordGenerator(); // Creating the single instance
+					        }
+					}
 				}
 				return _instance;
 			}
@@ -35,10 +42,10 @@ namespace Design_Patterns_Labb1_PasswordGenerator
 		{
 			List<ICharacterSet> characterSets = new List<ICharacterSet>();
 
-			if (includeLowercase) characterSets.Add(CharacterSetFactory.CreateCharacterSet(CharacterSetType.Lowercase)); // Factory Method
-			if (includeUppercase) characterSets.Add(CharacterSetFactory.CreateCharacterSet(CharacterSetType.Uppercase)); // Factory Method
-			if (includeDigits) characterSets.Add(CharacterSetFactory.CreateCharacterSet(CharacterSetType.Digits)); // Factory Method
-			if (includeSpecial) characterSets.Add(CharacterSetFactory.CreateCharacterSet(CharacterSetType.Special)); // Factory Method
+			if (includeLowercase) characterSets.Add(CharacterSetFactory.CreateCharacterSet(CharacterSetType.Lowercase)); // Factory Methods
+			if (includeUppercase) characterSets.Add(CharacterSetFactory.CreateCharacterSet(CharacterSetType.Uppercase)); 
+			if (includeDigits) characterSets.Add(CharacterSetFactory.CreateCharacterSet(CharacterSetType.Digits));
+			if (includeSpecial) characterSets.Add(CharacterSetFactory.CreateCharacterSet(CharacterSetType.Special)); 
 
 			if (characterSets.Count == 0)
 			{
